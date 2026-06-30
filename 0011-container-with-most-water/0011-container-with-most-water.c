041ba73a3ed246d99f45f353bb48b27c0a06743b
MayukhFired <ghoshmayukh175@gmail.com>
2026-06-30 19:53:10 +0530

    Time: 0 ms (100%), Space: 14.8 MB (49.33%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.c
+++ 0011-container-with-most-water/0011-container-with-most-water.c
@@ -1,22 +1,18 @@
-#define Max(a , b)  (a > b ? a : b);
-#define Min(a , b)  (a < b ? a : b);
-
 int maxArea(int* height, int heightSize) {
     int left = 0;
     int right = heightSize - 1;
-    int current_water = 0;
-    int result = 0;
+    int ans = INT_MIN;
 
-    while(left <= right){
-        current_water = (right - left) * Min(height[right] , height[left]);
-        result = Max(result , current_water);
-
+    while(left < right){
         if(height[left] < height[right]){
+            int temp = (right - left) * height[left];
+            ans = (ans > temp ? ans : temp);
             left++;
         }else{
+            int temp = (right - left) * height[right];
+            ans = (ans > temp ? ans : temp);
             right--;
         }
-    }
-
-    return result;
+    }    
+    return ans;
 }
