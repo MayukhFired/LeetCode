7954afb0fc7cdd3ef040d87d3e22206a6fcf4d2c
MayukhFired <ghoshmayukh175@gmail.com>
2026-07-01 23:09:39 +0530

    Time: 0 ms (100%), Space: 9.3 MB (48.69%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.c
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -4,13 +4,10 @@ int reverse(int x){
         int temp = x % 10;
         x = x / 10;
 
-        if(result > INT_MAX / 10 || (result == INT_MAX / 10 && temp > 7)){
-            return 0;
-        }
-
-        if(result < INT_MIN /10 || (result == INT_MIN / 10 && temp < -8)){
-            return 0;
-        }
+       if(result > INT_MAX / 10 || result < INT_MIN /10){
+        return 0;
+       }
+       
         result = (result * 10) + temp;
     }
 
