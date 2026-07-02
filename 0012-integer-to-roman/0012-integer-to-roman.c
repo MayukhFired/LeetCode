d8422d9c7cd597405bca2904e62b75a80e95ced3
MayukhFired <ghoshmayukh175@gmail.com>
2026-07-02 14:28:41 +0530

    Time: 5 ms (10.84%), Space: 10.1 MB (9.05%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.c
+++ 0012-integer-to-roman/0012-integer-to-roman.c
@@ -1,20 +1,15 @@
 char* intToRoman(int num) {
     int values[] = {1000 , 900 , 500 , 400 , 100 , 90 , 50 , 40 ,10 , 9 , 5 , 4 , 1};
     char* symbols[] = { "M" , "CM" , "D", "CD" , "C" , "XC" , "L" , "XL" , "X" , "IX" ,"V" , "IV" , "I"};
-
-    char* result = (char*)malloc(16 * sizeof(char));
+    char* result = (char*)malloc(20 * sizeof(char));
+    result[0] = '\0';
     if(result == NULL) return NULL;
-    int len = 0;
     for(int i = 0 ; i < 13; i++){
         while(num >= values[i]){
-            char *s = symbols[i];
-            while(*s){
-                result[len++] = *s++;
-            }
+            strcat(result , symbols[i]);
             num -= values[i];
         }
     }
-    result[len] = '\0';
     return result;
     free(result);
 }
