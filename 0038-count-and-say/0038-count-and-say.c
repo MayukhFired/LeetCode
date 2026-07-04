3fd01b713539f88b06f91b32e41157de18a09649
MayukhFired <ghoshmayukh175@gmail.com>
2026-07-04 23:35:13 +0530

    Time: 7 ms (33.82%), Space: 15.6 MB (5.36%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.c
@@ -0,0 +1,31 @@
+char* countAndSay(int n) {
+    char* current = (char*)malloc(2* sizeof(char));
+    strcpy(current , "1");
+
+    if(n == 1){
+        return current;
+    }
+
+    for(int i = 2; i <= n; i++){
+        char* next_str = (char*)malloc(50000* sizeof(char));
+        int index = 0;
+        int len = strlen(current);
+        int j = 0;
+
+        while(j < len){
+            int count = 1;
+            while(j + 1 < len && current[j] == current[j + 1]){
+                count++;
+                j++;
+            }
+
+            index += sprintf(&next_str[index] , "%d%c" , count , current[j]);
+            j++;
+        }
+        next_str[index] = '\0';
+        free(current);
+        current = next_str;
+    }
+
+    return current;
+}
