dfcaa9f7d382be2ae20cbf76015ca56938471d23
MayukhFired <ghoshmayukh175@gmail.com>
2026-07-09 21:49:50 +0530

    Time: 11 ms (87.68%), Space: 9.1 MB (17.38%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.c
@@ -0,0 +1,34 @@
+int compare(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int threeSumClosest(int* nums, int numsSize, int target) {
+    int result = nums[0] + nums[1] + nums[2];
+
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    for(int i = 0; i < numsSize; i++){
+        int left = i + 1;
+        int right = numsSize - 1;
+
+        while(left < right){
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if(sum == target){
+                return sum;
+            }
+
+            if(abs(target - sum) < abs(target - result)){
+                result = sum;
+            }
+
+            if(sum < target){
+                left++;
+            }else{
+                right--;
+            }
+        }
+    }
+
+    return result;
+}
