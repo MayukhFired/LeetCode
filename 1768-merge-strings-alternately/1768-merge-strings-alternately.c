d9cf6707ac8b297c43088d175d897fd138cfa823
MayukhFired <ghoshmayukh175@gmail.com>
2026-07-10 01:10:42 +0530

    Time: 0 ms (100%), Space: 8.7 MB (75.51%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.c
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.c
@@ -1,55 +1,34 @@
 
 
-// char * mergeAlternately(char * word1, char * word2){
-//     int len_1 = strlen(word1);
-//     int len_2 = strlen(word2);
-//     int returnsize = len_1 + len_2 + 1;
-//     char* result = malloc(returnsize *sizeof(char));
-
-//     int i = 0 , j = 0 , k = 0;
-
-//     while(i < len_1 || j < len_2){
-//         if(i < len_1){
-//             result[k++] = word1[i++];
-//         }
-
-//         if(j < len_2){
-//             result[k++] = word2[j++];
-//         }
-//     }
-// // Another method to solve it 
-// //    while(i < len_1){
-// //         result[k++] = word1[i++];
-// //     }
-
-// //     while(j < len_2){
-// //         result[k++] = word2[j++];
-// //     }
-
-//     result[k] = '\0';
-
-//     return result;
-// }
-
-
 char * mergeAlternately(char * word1, char * word2){
-    int len1 = strlen(word1);
-    int len2 = strlen(word2);
-    int returnSize = len1+len2;
-    char* result = malloc((returnSize+1)*sizeof(char));
-    int i=0,j=0,k=0;
-    while(i<len1 || j<len2)
-    {
-        if(i<len1)
-        {
+    int len_1 = strlen(word1);
+    int len_2 = strlen(word2);
+    int returnsize = len_1 + len_2 + 1;
+    char* result = malloc(returnsize *sizeof(char));
+
+    int i = 0 , j = 0 , k = 0;
+
+    while(i < len_1 || j < len_2){
+        if(i < len_1){
             result[k++] = word1[i++];
         }
-        if(j<len2)
-        {
+
+        if(j < len_2){
             result[k++] = word2[j++];
         }
     }
-    result[k] = '\0';
-    return result;
+// Another method to solve it 
+//    while(i < len_1){
+//         result[k++] = word1[i++];
+//     }
 
+//     while(j < len_2){
+//         result[k++] = word2[j++];
+//     }
+
+    result[k] = '\0';
+
+    return result;
 }
+
+
