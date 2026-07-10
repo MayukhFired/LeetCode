9591d8e1f83f6bef6a9b1ab3ef7b2710724b561b
MayukhFired <ghoshmayukh175@gmail.com>
2026-07-10 23:08:50 +0530

    Time: 0 ms (100%), Space: 8.8 MB (89.87%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.c
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -1,34 +1,27 @@
 char* longestCommonPrefix(char** strs, int strsSize) {
-    // Handle base case
-    if (strsSize == 0 || strs == NULL) {
-        char *empty = (char*)malloc(1);
+    if(strs == NULL || strsSize == 0){
+        char* empty = (char*)malloc(1);
         empty[0] = '\0';
         return empty;
     }
 
-    char *compare = (char*)malloc(201 * sizeof(char));
-    
-    // Start by assuming the prefix is the entire first word
-    int min_len = strlen(strs[0]); 
+    int min_len = strlen(strs[0]);
+    char* compare = (char*)malloc(128* sizeof(char));
 
-    // Loop through the rest of the words safely (up to strsSize)
-    for (int i = 1; i < strsSize; i++) {
+
+    for(int i = 1; i < strsSize; i++){
         int j = 0;
-        
-        // Count matches, but don't exceed the existing prefix length
-        while (j < min_len && strs[0][j] == strs[i][j]) {
+        while(j < min_len && strs[0][j] == strs[i][j]){
             j++;
         }
-        
-        // Shrink the prefix length to the smallest match found so far
-        min_len = j; 
+
+        min_len = j;
     }
 
-    // Now copy the finalized prefix into your allocated memory
-    for (int k = 0; k < min_len; k++) {
-        compare[k] = strs[0][k];
+    for(int i = 0; i < min_len; i++){
+        compare[i] = strs[0][i];
     }
-    
-    compare[min_len] = '\0'; // Crucial: Null-terminate the string!
+    compare[min_len] = '\0';
     return compare;
+
 }
