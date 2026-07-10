dc988abf0b9ca69899adfa597094bd3b0d1f7301
MayukhFired <ghoshmayukh175@gmail.com>
2026-07-10 23:09:36 +0530

    Time: 0 ms (100%), Space: 8.8 MB (68.78%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.c
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -1,27 +1,44 @@
+// char* longestCommonPrefix(char** strs, int strsSize) {
+//     if(strs == NULL || strsSize == 0){
+//         char* empty = (char*)malloc(1);
+//         empty[0] = '\0';
+//         return empty;
+//     }
+
+//     int min_len = strlen(strs[0]);
+//     char* compare = (char*)malloc(128* sizeof(char));
+
+
+//     for(int i = 1; i < strsSize; i++){
+//         int j = 0;
+//         while(j < min_len && strs[0][j] == strs[i][j]){
+//             j++;
+//         }
+
+//         min_len = j;
+//     }
+
+//     for(int i = 0; i < min_len; i++){
+//         compare[i] = strs[0][i];
+//     }
+//     compare[min_len] = '\0';
+//     return compare;
+
+// }
 char* longestCommonPrefix(char** strs, int strsSize) {
-    if(strs == NULL || strsSize == 0){
-        char* empty = (char*)malloc(1);
-        empty[0] = '\0';
-        return empty;
-    }
+    if (strsSize == 0)
+        return "";
 
-    int min_len = strlen(strs[0]);
-    char* compare = (char*)malloc(128* sizeof(char));
+    for (int i = 0; strs[0][i] != '\0'; i++) {
+        char ch = strs[0][i];
 
-
-    for(int i = 1; i < strsSize; i++){
-        int j = 0;
-        while(j < min_len && strs[0][j] == strs[i][j]){
-            j++;
+        for (int j = 1; j < strsSize; j++) {
+            if (strs[j][i] != ch || strs[j][i] == '\0') {
+                strs[0][i] = '\0';
+                return strs[0];
+            }
         }
-
-        min_len = j;
     }
 
-    for(int i = 0; i < min_len; i++){
-        compare[i] = strs[0][i];
-    }
-    compare[min_len] = '\0';
-    return compare;
-
+    return strs[0];
 }
