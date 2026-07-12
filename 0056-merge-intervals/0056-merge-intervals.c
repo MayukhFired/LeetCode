d6ce4800d7e7e7bd476903c312c0af52066ee833
MayukhFired <ghoshmayukh175@gmail.com>
2026-07-12 15:28:25 +0530

    Time: 0 ms (100%), Space: 18.7 MB (74.73%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.c
+++ 0056-merge-intervals/0056-merge-intervals.c
@@ -25,7 +25,7 @@ int** merge(int** intervals, int intervalsSize, int* intervalsColSize, int* retu
     merged[0] = (int*)malloc(2 * sizeof(int));
     merged[0][0] = intervals[0][0];
     merged[0][1] = intervals[0][1];
-    (*returnColumnSizes)[count] = 2;
+    (*returnColumnSizes)[0] = 2;
     count++;
 
     for(int i = 1; i < intervalsSize; i++){
