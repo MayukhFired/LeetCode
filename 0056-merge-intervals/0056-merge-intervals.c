2b0f189976a3b960420c4aba5caa8123a45845a6
MayukhFired <ghoshmayukh175@gmail.com>
2026-07-12 15:22:47 +0530

    Time: 8 ms (12.39%), Space: 18.7 MB (74.73%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.c
@@ -0,0 +1,52 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+
+ int compare(const void *a , const void *b){
+    int startA = (*(int**)a)[0];
+    int startB = (*(int**)b)[0];
+
+    if(startA < startB) return -1;
+    if(startA > startB) return 1;
+    return 0;
+ }
+
+int** merge(int** intervals, int intervalsSize, int* intervalsColSize, int* returnSize, int** returnColumnSizes) {
+    if(intervalsSize <= 0){
+        *returnSize = 0;
+        return NULL;
+    }
+
+    qsort(intervals , intervalsSize , sizeof(int*) , compare);
+
+    int** merged = (int**)malloc(intervalsSize * sizeof(int*));
+    *returnColumnSizes = (int*)malloc(intervalsSize * sizeof(int));
+
+    int count = 0;
+    merged[count] = (int*)malloc(2 * sizeof(int));
+    merged[count][0] = intervals[0][0];
+    merged[count][1] = intervals[0][1];
+    (*returnColumnSizes)[count] = 2;
+    count++;
+
+    for(int i = 1; i < intervalsSize; i++){
+        int last = merged[count-1][1];
+
+        if(intervals[i][0] <= last){
+            if(intervals[i][1] > last){
+                merged[count - 1][1] = intervals[i][1];
+            }
+        }else{
+            merged[count] = (int*)malloc(2 * sizeof(int));
+            merged[count][0] = intervals[i][0];
+            merged[count][1] = intervals[i][1];
+            (*returnColumnSizes)[count] = 2;
+            count++;
+        }
+    }
+
+    *returnSize = count;
+    return merged;
+}
