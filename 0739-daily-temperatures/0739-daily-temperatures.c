86a214c958e23e8ce981279254e0aa3d57b6788e
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-07-15 15:24:09 +0530

    Time: 8 ms (78.63%), Space: 71.7 MB (6.83%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.c
+++ 0739-daily-temperatures/0739-daily-temperatures.c
@@ -4,7 +4,7 @@
 int* dailyTemperatures(int* temperatures, int temperaturesSize, int* returnSize) {
     *returnSize = temperaturesSize;
     int* result = (int*)calloc(temperaturesSize , sizeof(int));
-    int* stack = (int*)malloc(temperaturesSize * sizeof(int));
+    int* stack = (int*)calloc(temperaturesSize , sizeof(int));
 
     int top = -1;
     for(int i = 0; i < temperaturesSize; i++){
