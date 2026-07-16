4ac249ac9441cdeac75a93a28a2e940f8d0d2294
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-07-16 15:03:03 +0530

    Time: 126 ms (40%), Space: 30.6 MB (46.67%) - LeetHub

---
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.c
@@ -0,0 +1,59 @@
+// int sort(int* prefixGcd , int length){
+//     for(int i = 0; i < length; i++){
+//         for(int j = 0; j < length - i - 1; j++){
+//             if(prefixGcd[j+1] < prefixGcd[j]){
+//                 int temp = prefixGcd[j];
+//                 prefixGcd[j + 1] = prefixGcd[j];
+//                 prefixGcd[j + 1] = temp;
+//             }
+//         }
+//     }
+
+//     return prefixGcd;
+// }
+
+int compare(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int gcd(int a , int b){
+    while(b != 0){
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    return a;
+}
+
+long long gcdSum(int* nums, int numsSize) {
+    int* prefixGcd = (int*)malloc(numsSize * sizeof(int));
+    int max = nums[0];
+    int index = 0;
+    for(int i = 0; i < numsSize; i++){
+        if(max < nums[i]){
+            max = nums[i];
+            prefixGcd[index++] = gcd(nums[i] , max);
+        }else{
+            prefixGcd[index++] = gcd(nums[i] , max);
+        }
+    }
+
+    // sort(prefixGcd , numsSize);
+    qsort(prefixGcd, numsSize, sizeof(int), compare);
+    int left = 0;
+    int right = numsSize - 1;
+    long long sum = 0;
+    while(left <= right){
+        if(left == right){
+        break;
+        }else{
+            sum += gcd(prefixGcd[left] , prefixGcd[right]);
+        }
+        left++;
+        right--;
+    }
+
+    free(prefixGcd);
+    return sum;
+}
