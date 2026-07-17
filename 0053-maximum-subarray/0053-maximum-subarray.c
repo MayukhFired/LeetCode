609aabd204e1b717019419fa2f93ea5e2e23cfad
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-07-17 23:46:46 +0530

    Time: 4 ms (9.85%), Space: 15.5 MB (53.29%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.c
@@ -0,0 +1,25 @@
+// int compare(const void *a , const void* b){
+//     int val_a = *(const int *)a;
+//     int val_b = *(const int *)b;
+//     return (val_a > val_b) - (val_a < val_b);
+// }
+
+int maxSubArray(int* nums, int numsSize) {
+    int max_sum = nums[0];
+    int current_sum = nums[0];
+
+    for(int i = 1; i < numsSize; i++){
+        if(current_sum < 0){
+            current_sum = nums[i];
+        }else{
+            current_sum += nums[i];
+        }
+            if(max_sum < current_sum){
+            max_sum = current_sum;
+        }
+    }
+
+    
+
+    return max_sum;
+}
