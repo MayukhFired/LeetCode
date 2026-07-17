9b240f6b05850ac59808106aa8088758f545f47e
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-07-17 23:47:38 +0530

    Time: 0 ms (100%), Space: 15.5 MB (13.34%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.c
+++ 0053-maximum-subarray/0053-maximum-subarray.c
@@ -1,25 +1,35 @@
-// int compare(const void *a , const void* b){
-//     int val_a = *(const int *)a;
-//     int val_b = *(const int *)b;
-//     return (val_a > val_b) - (val_a < val_b);
-// }
+// // int compare(const void *a , const void* b){
+// //     int val_a = *(const int *)a;
+// //     int val_b = *(const int *)b;
+// //     return (val_a > val_b) - (val_a < val_b);
+// // }
 
-int maxSubArray(int* nums, int numsSize) {
-    int max_sum = nums[0];
-    int current_sum = nums[0];
+// int maxSubArray(int* nums, int numsSize) {
+//     int max_sum = nums[0];
+//     int current_sum = nums[0];
 
-    for(int i = 1; i < numsSize; i++){
-        if(current_sum < 0){
-            current_sum = nums[i];
-        }else{
-            current_sum += nums[i];
-        }
-            if(max_sum < current_sum){
-            max_sum = current_sum;
-        }
-    }
+//     for(int i = 1; i < numsSize; i++){
+//         if(current_sum < 0){
+//             current_sum = nums[i];
+//         }else{
+//             current_sum += nums[i];
+//         }
+//             if(max_sum < current_sum){
+//             max_sum = current_sum;
+//         }
+//     }
 
     
 
-    return max_sum;
+//     return max_sum;
+// }
+
+int maxSubArray(int* nums, int numsSize) {
+    int res = nums[0];
+    int maxEnding = nums[0];
+    for (int i = 1; i < numsSize; i++) {
+        maxEnding = (maxEnding + nums[i] > nums[i]) ? maxEnding + nums[i] : nums[i];
+        res = (res > maxEnding) ? res : maxEnding;
+    }
+    return res;
 }
