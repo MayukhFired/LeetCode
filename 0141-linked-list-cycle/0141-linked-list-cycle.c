1f09a006be7c10b089ae63c81a380a0bb9a55131
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-07-20 14:24:39 +0530

    Time: 7 ms (92.39%), Space: 11.8 MB (85.44%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.c
+++ 0141-linked-list-cycle/0141-linked-list-cycle.c
@@ -1,3 +1,29 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     struct ListNode *next;
+//  * };
+//  */
+// bool hasCycle(struct ListNode *head) {
+
+//     if(head == NULL){
+//         return false;
+//     }
+//     struct ListNode *slow = head;
+//     struct ListNode *fast = head;
+
+//     while(fast != NULL && fast->next != NULL){
+//         slow = slow->next;
+//         fast = fast->next->next;
+
+//         if(slow == fast){
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -6,21 +32,15 @@
  * };
  */
 bool hasCycle(struct ListNode *head) {
-
-    if(head == NULL){
-        return false;
-    }
-    struct ListNode *slow = head;
-    struct ListNode *fast = head;
-
-    while(fast != NULL && fast->next != NULL){
-        slow = slow->next;
-        fast = fast->next->next;
-
-        if(slow == fast){
-            return true;
-        }
-    }
-
-    return false;
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+    if(head == NULL)
+        return 0;
+    do{
+        if(fast->next == NULL || fast->next->next == NULL)
+            return 0;
+            fast = fast->next->next;
+            slow = slow->next;
+    }while(fast!=slow);
+    return 1;
 }
