d320b29f0d6955dc18b38327ff48ab4c89c45a7d
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-07-23 19:56:56 +0530

    Time: 4 ms (15.25%), Space: 31.9 MB (76.27%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.py
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.py
@@ -2,7 +2,7 @@ class Solution:
     def uniqueXorTriplets(self, nums: List[int]) -> int:
         n = len(nums)
         ans = 1
-        if n == 1 or n == 2:
+        if n <= 2:
             return n
         while ans <= n:
             ans *= 2
