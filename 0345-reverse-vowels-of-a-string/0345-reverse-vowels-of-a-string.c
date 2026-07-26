6daea1a3b412c64cfec06d536524d105c25cc272
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-07-26 15:03:07 +0530

    Time: 0 ms (100%), Space: 9.5 MB (76.48%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.c
@@ -0,0 +1,23 @@
+bool isvowel(char c){
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c== 'u'||
+           c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+}
+
+char* reverseVowels(char* s) {
+    int left = 0;
+    int right = strlen(s) - 1;
+    while(left < right){
+        while(left < right && !isvowel(s[left])) left++;
+        while(left < right && !isvowel(s[right])) right--;
+
+        if(left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+        }
+
+        left++;
+        right--;
+    }
+    return s;
+}
