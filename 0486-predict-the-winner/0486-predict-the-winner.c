9f6305ac711c26061fcdafc741418a909be94ef7
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-08-01 12:45:19 +0530

    Time: 0 ms (100%), Space: 8.7 MB (53.85%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.c
+++ 0486-predict-the-winner/0486-predict-the-winner.c
@@ -1,7 +1,7 @@
 #define max(a, b) ((a) > (b) ? (a) : (b))
 #define min(a, b) ((a) < (b) ? (a) : (b))
-
-int solve(int i , int j , int* nums , int memo[21][21]){
+// int solve(int i , int j , int* nums , int memo[21][21]){
+    int solve(int i , int j , int* nums , int memo[21][21]){
     if(i > j){
         return 0;
     }
@@ -14,8 +14,11 @@ int solve(int i , int j , int* nums , int memo[21][21]){
         return memo[i][j];
     }
 
-    int take_1 = nums[i] + min(solve(i + 2 , j , nums , memo) , solve(i + 1 , j - 1, nums , memo));
-    int take_2 = nums[j] + min(solve(i , j - 2 , nums , memo) , solve(i + 1 , j - 1 ,nums , memo));
+    // int take_1 = nums[i] + min(solve(i + 2 , j , nums , memo) , solve(i + 1 , j - 1, nums , memo));
+    // int take_2 = nums[j] + min(solve(i , j - 2 , nums , memo) , solve(i + 1 , j - 1 ,nums , memo));
+    // return memo[i][j] = max(take_1 , take_2);
+    int take_1 = nums[i] - solve(i + 1 , j , nums , memo);
+    int take_2 = nums[j] - solve(i , j - 1 , nums , memo);
     return memo[i][j] = max(take_1 , take_2);
 }
 
@@ -27,11 +30,12 @@ bool predictTheWinner(int* nums, int numsSize) {
             memo[i][j] = -1;
         }
     }
-    for(int k = 0; k < numsSize; k++){
-        sum += nums[k];
-    }
+    // for(int k = 0; k < numsSize; k++){
+    //     sum += nums[k];
+    // }
 
-    int p_1 = solve(0 , numsSize - 1 , nums, memo);
-    int p_2 = sum - p_1;
-    return (p_1 >= p_2);
+    // int p_1 = solve(0 , numsSize - 1 , nums, memo);
+    // int p_2 = sum - p_1;
+    // return (p_1 >= p_2);
+    return (solve(0 , numsSize - 1 , nums, memo) >= 0);
 }
