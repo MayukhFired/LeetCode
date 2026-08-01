d10768c00a92cedf2d912ef9ad0785803a61a1bc
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-08-01 12:32:23 +0530

    Time: 0 ms (100%), Space: 19.7 MB (33.61%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,15 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        @lru_cache(None)
+        def solve(i: int , j: int) -> int:
+            if i > j:
+                return 0
+            if i == j:
+                return nums[i]
+            take_1 = nums[i] + min(solve(i + 2, j) , solve(i + 1 , j - 1))
+            take_2 = nums[j] + min(solve(i , j - 2) , solve(i + 1 , j - 1))
+            return max(take_1 , take_2)
+        total = sum(nums)
+        p_1 = solve(0 , len(nums) - 1)
+        p_2 = total - p_1
+        return p_1 >= p_2
