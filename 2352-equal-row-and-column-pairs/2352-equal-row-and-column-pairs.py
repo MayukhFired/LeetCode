ca30da2ebfa822ea94e56ba9ac389fddf85a10c8
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-08-08 01:02:43 +0530

    Time: 71 ms (28.6%), Space: 24.6 MB (55.56%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,12 +1,35 @@
+#Brute force solution of the problem
+# class Solution:
+#     def equalPairs(self, grid: List[List[int]]) -> int:
+#         count = 0
+#         n = len(grid)
+#         for r in range(n):
+#             for c in range(n):
+#                 is_equal = True
+#                 for i in range(n):
+#                     if grid[r][i] != grid[i][c]:
+#                         is_equal = False
+#                 count += is_equal
+#         return count
+
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        count = 0
+        BASE = 131
+        MOD = 1_000_000_007
         n = len(grid)
-        for r in range(n):
-            for c in range(n):
-                is_equal = True
-                for i in range(n):
-                    if grid[r][i] != grid[i][c]:
-                        is_equal = False
-                count += is_equal
+        row_hashes = [0] * n
+        col_hashes = [0] * n
+        for i in range(n):
+            RowH = 0
+            ColH = 0
+            for j in range(n):
+                RowH = (RowH * BASE + grid[i][j]) % MOD
+                ColH = (ColH * BASE + grid[j][i]) % MOD
+            row_hashes[i] = RowH
+            col_hashes[i] = ColH
+        count = 0
+        for k in range(n):
+            for m in range(n):
+                if row_hashes[k] == col_hashes[m]:
+                    count += 1
         return count
