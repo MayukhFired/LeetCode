33ccc4a9bb71b5dbf2e424c79b491774b7461fc0
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-08-08 00:56:50 +0530

    Time: 8 ms (98.96%), Space: 13.9 MB (76.11%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.c
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.c
@@ -1,17 +1,46 @@
-int equalPairs(int** grid, int gridSize, int* gridColSize) {
+//Brute force solution of this problem 
+// int equalPairs(int** grid, int gridSize, int* gridColSize) {
+//     int count = 0;
+//     for(int r = 0; r < gridSize; r++){
+//         for(int c = 0; c < gridSize; c++){
+//             int is_equal = true;
+//             for(int i = 0; i < gridSize; i++){
+//                 if(grid[r][i] != grid[i][c]){
+//                     is_equal = false;
+//                 }
+//             }
+//             if(is_equal){
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// }
+
+#define BASE 131
+#define MOD 1000000007
+
+int equalPairs(int** grid, int gridSize, int* gridColSize){
+    int RowHashes[gridSize];
+    int ColHashes[gridSize];
+    for(int i = 0; i < gridSize; i++){
+        long long RowH = 0;
+        long long ColH = 0;
+        for(int j = 0; j < gridSize; j++){
+            RowH = (RowH * BASE + grid[i][j]) % MOD;
+            ColH = (ColH * BASE + grid[j][i]) % MOD;
+        }
+        RowHashes[i] = RowH;
+        ColHashes[i] = ColH;
+    }
     int count = 0;
-    for(int r = 0; r < gridSize; r++){
-        for(int c = 0; c < gridSize; c++){
-            int is_equal = true;
-            for(int i = 0; i < gridSize; i++){
-                if(grid[r][i] != grid[i][c]){
-                    is_equal = false;
-                }
-            }
-            if(is_equal){
+    for(int i = 0; i < gridSize; i++){
+        for(int j = 0; j < gridSize; j++){
+            if(RowHashes[i] == ColHashes[j]){
                 count++;
             }
         }
     }
+
     return count;
 }
