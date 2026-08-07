3f9f31c99fa31ea2c824247bdb865f6ad2f0c71e
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-08-08 01:07:21 +0530

    Time: 11 ms (91.13%), Space: 24.6 MB (55.61%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -12,24 +12,30 @@
 #                 count += is_equal
 #         return count
 
+#Better approach
+# class Solution:
+#     def equalPairs(self, grid: List[List[int]]) -> int:
+#         BASE = 131
+#         MOD = 1_000_000_007
+#         n = len(grid)
+#         row_hashes = [0] * n
+#         col_hashes = [0] * n
+#         for i in range(n):
+#             RowH = 0
+#             ColH = 0
+#             for j in range(n):
+#                 RowH = (RowH * BASE + grid[i][j]) % MOD
+#                 ColH = (ColH * BASE + grid[j][i]) % MOD
+#             row_hashes[i] = RowH
+#             col_hashes[i] = ColH
+#         count = 0
+#         for k in range(n):
+#             for m in range(n):
+#                 if row_hashes[k] == col_hashes[m]:
+#                     count += 1
+#         return count
+
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        BASE = 131
-        MOD = 1_000_000_007
-        n = len(grid)
-        row_hashes = [0] * n
-        col_hashes = [0] * n
-        for i in range(n):
-            RowH = 0
-            ColH = 0
-            for j in range(n):
-                RowH = (RowH * BASE + grid[i][j]) % MOD
-                ColH = (ColH * BASE + grid[j][i]) % MOD
-            row_hashes[i] = RowH
-            col_hashes[i] = ColH
-        count = 0
-        for k in range(n):
-            for m in range(n):
-                if row_hashes[k] == col_hashes[m]:
-                    count += 1
-        return count
+        row_counts = Counter(tuple(row) for row in grid)
+        return sum(row_counts[col] for col in zip(*grid))
