1a5c04276370cf42225f388f40b91b4f1549d55d
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-08-14 11:13:45 +0530

    Time: 0 ms (100%), Space: 8.3 MB (89.56%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.c
@@ -0,0 +1,21 @@
+bool isPerfectSquare(int num) {
+    if(num < 1){
+        return false;
+    }else if(num == 1){
+        return true;
+    }
+    long long low = 1;
+    long long high = num;
+    while(low <= high){
+        long long mid = low + (high - low) / 2;
+        long long square = mid * mid;
+        if(square == num){
+            return true;
+        }else if(square > num){
+            high = mid - 1;
+        }else{
+            low = mid + 1;
+        }
+    }
+    return false;
+}
