8a92535c3cbd0f036a341b0cf566d9d45c47edb2
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-08-22 23:06:48 +0530

    Time: 0 ms (100%), Space: 14.9 MB (85.77%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -23,8 +23,33 @@ private:
     }
 public:
     vector<int> rightSideView(TreeNode* root) {
+    //     vector<int> result;
+    //     dfs(root , 0 , result);
+    //     return result;
+    // }
+        if(!root){
+            return {};
+        }
         vector<int> result;
-        dfs(root , 0 , result);
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int levelSize = q.size();
+            for(int i = 0; i < levelSize; i++){
+                TreeNode* curr = q.front();
+                q.pop();
+
+                if(i == levelSize - 1){
+                    result.push_back(curr->val);
+                }
+                if(curr->left){
+                    q.push(curr->left);
+                }
+                if(curr->right){
+                    q.push(curr->right);
+                }
+            }
+        }
         return result;
     }
 };
