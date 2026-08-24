0fb9e0dc40971c9762d588303e88589837e85434
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-08-24 21:55:48 +0530

    Time: 0 ms (100%), Space: 106.9 MB (96.8%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -11,35 +11,67 @@
  */
 class Solution {
 public:
-    int maxLevelSum(TreeNode* root) {
-        if(!root){
-            return 0;
+    // int maxLevelSum(TreeNode* root) {
+        //Solved using BFS method
+        // if(!root){
+        //     return 0;
+        // }
+        // int max_sum = INT_MIN;
+        // int max_level = 1;
+        // int current_level = 1;
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int level_size = q.size();
+        //     int level_sum = 0;
+        //     for(int i = 0; i < level_size; i++){
+        //         TreeNode* node = q.front();
+        //         q.pop();
+        //         level_sum += node->val;
+        //         if(node->left){
+        //             q.push(node->left);
+        //         }
+        //         if(node->right){
+        //             q.push(node->right);
+        //         }
+        //     }
+        //     if(level_sum > max_sum){
+        //         max_sum = level_sum;
+        //         max_level = current_level;
+        //     }
+        //     current_level++;
+        // }
+        // return max_level;
+        //Solved using DFS method
+    private:
+        void dfs(TreeNode* node , int level , vector<int>& level_sums){
+            if(!node){
+                return;
+            }
+            if(level == level_sums.size()){
+                level_sums.push_back(node->val);
+            }else{
+                level_sums[level] += node->val;
+            }
+            dfs(node->left , level + 1 , level_sums);
+            dfs(node->right , level + 1 , level_sums);
         }
-        int max_sum = INT_MIN;
-        int max_level = 1;
-        int current_level = 1;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            int level_size = q.size();
-            int level_sum = 0;
-            for(int i = 0; i < level_size; i++){
-                TreeNode* node = q.front();
-                q.pop();
-                level_sum += node->val;
-                if(node->left){
-                    q.push(node->left);
-                }
-                if(node->right){
-                    q.push(node->right);
+    public:
+        int maxLevelSum(TreeNode* root){
+            if(!root){
+                return 0;
+            }
+            vector<int> level_sums;
+            dfs(root , 0 , level_sums);
+            int max_sum = INT_MIN;
+            int max_level = 1;
+            for(int i = 0; i < level_sums.size(); i++){
+                if(level_sums[i] > max_sum){
+                    max_sum = level_sums[i];
+                    max_level = i + 1;
                 }
             }
-            if(level_sum > max_sum){
-                max_sum = level_sum;
-                max_level = current_level;
-            }
-            current_level++;
+            return max_level;
         }
-        return max_level;
-    }
+    
 };
