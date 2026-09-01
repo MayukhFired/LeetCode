f1a622913c4e0a73ec990af44e65141e87554802
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-09-01 20:24:50 +0530

    Time: 5 ms (27.48%), Space: 19.3 MB (33.57%) - LeetHub

---
+++ 0390-elimination-game/0390-elimination-game.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lastRemaining(self, n: int) -> int:
+        head = 1
+        step = 1
+        remaining = n
+        left = True
+        while remaining > 1:
+            if left or remaining % 2 == 1:
+                head = head + step
+            
+            remaining = remaining // 2
+            step *= 2
+            left = not left
+        return head
