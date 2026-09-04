08b9c4d53fdaec4115c4362030421bb034ed0e2d
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-09-04 20:01:45 +0530

    Time: 0 ms (100%), Space: 23.8 MB (30%) - LeetHub

--- 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.c
+++ 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.c
@@ -1,35 +1,55 @@
 int smallestBalancedIndex(int* nums, int numsSize) {
-    if(numsSize <= 0 || nums == NULL){
-        return -1;
-    }
+    // if(numsSize <= 0 || nums == NULL){
+    //     return -1;
+    // }
     
-    long long total_sum = 0;
+    // long long total_sum = 0;
+    // for(int i = 0; i < numsSize; i++){
+    //     total_sum += nums[i];
+    // }
+
+    // long long maxpossible_sum = total_sum;
+
+    // long long right_product = 1;
+    // int result = -1;
+
+    // int is_overflowed = 0;
+    // long long safe_cap = 1000000000000000LL;
+    // for(int i = numsSize - 1; i >= 0; i--){
+    //     total_sum -= nums[i];
+
+    //     if(!is_overflowed && total_sum == right_product){
+    //         result = i;
+    //     }
+
+    //     if(!is_overflowed){
+    //         if(nums[i] != 0 && right_product > safe_cap / abs(nums[i])){
+    //             is_overflowed = 1;
+    //         }else{
+    //             right_product *= nums[i];
+    //         }
+    //     }
+    // }
+
+    // return result;
+
+    long long s = 0;
+    __int128 p = 1;
+
     for(int i = 0; i < numsSize; i++){
-        total_sum += nums[i];
+        s += nums[i];
     }
 
-    long long maxpossible_sum = total_sum;
-
-    long long right_product = 1;
-    int result = -1;
-
-    int is_overflowed = 0;
-    long long safe_cap = 1000000000000000LL;
     for(int i = numsSize - 1; i >= 0; i--){
-        total_sum -= nums[i];
-
-        if(!is_overflowed && total_sum == right_product){
-            result = i;
+        s -= nums[i];
+        if(s == p){
+            return i;
         }
 
-        if(!is_overflowed){
-            if(nums[i] != 0 && right_product > safe_cap / abs(nums[i])){
-                is_overflowed = 1;
-            }else{
-                right_product *= nums[i];
-            }
+        p *= nums[i];
+        if(p >= s){
+            break;
         }
     }
-
-    return result;
+    return -1;
 }
