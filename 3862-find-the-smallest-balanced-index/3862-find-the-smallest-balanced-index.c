fd6b908796a01b1352f1b42caddf3ed99f8637d7
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-09-04 19:57:06 +0530

    Time: 4 ms (50%), Space: 23.6 MB (90%) - LeetHub

---
+++ 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.c
@@ -0,0 +1,35 @@
+int smallestBalancedIndex(int* nums, int numsSize) {
+    if(numsSize <= 0 || nums == NULL){
+        return -1;
+    }
+    
+    long long total_sum = 0;
+    for(int i = 0; i < numsSize; i++){
+        total_sum += nums[i];
+    }
+
+    long long maxpossible_sum = total_sum;
+
+    long long right_product = 1;
+    int result = -1;
+
+    int is_overflowed = 0;
+    long long safe_cap = 1000000000000000LL;
+    for(int i = numsSize - 1; i >= 0; i--){
+        total_sum -= nums[i];
+
+        if(!is_overflowed && total_sum == right_product){
+            result = i;
+        }
+
+        if(!is_overflowed){
+            if(nums[i] != 0 && right_product > safe_cap / abs(nums[i])){
+                is_overflowed = 1;
+            }else{
+                right_product *= nums[i];
+            }
+        }
+    }
+
+    return result;
+}
