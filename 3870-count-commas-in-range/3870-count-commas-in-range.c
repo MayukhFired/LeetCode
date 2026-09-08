d4afb6289c61d2412ddc037b3cff0876b1bed459
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-09-08 06:45:32 +0530

    Time: 0 ms (100%), Space: 9.2 MB (31.17%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.c
+++ 3870-count-commas-in-range/3870-count-commas-in-range.c
@@ -1,9 +1,11 @@
 int countCommas(int n) {
-    int com = 0;
-    for(int i = 0; i <= n; i++){
-        if(i > 999){
-            com++;
-        }
-    }
-    return com;
+    // int com = 0;
+    // for(int i = 0; i <= n; i++){
+    //     if(i > 999){
+    //         com++;
+    //     }
+    // }
+    // return com;
+
+    return fmax((n - 999) , 0);
 }
