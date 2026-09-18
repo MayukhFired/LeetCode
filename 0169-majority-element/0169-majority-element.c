da50ecadf4530bf657c0436dad9bcfa05aa9cd54
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-09-18 23:04:49 +0530

    Time: 5 ms (4.83%), Space: 12.8 MB (7.56%) - LeetHub

--- 0169-majority-element/0169-majority-element.c
+++ 0169-majority-element/0169-majority-element.c
@@ -1,11 +1,18 @@
+int compare(const void* a , const void* b){
+    return (*(int*)a - *(int*)b);
+}
+
 int majorityElement(int* nums, int numsSize) {
-    int count = 0;
-    int candidate = 0;
-    for(int i = 0; i < numsSize; i++){
-        if(count == 0){
-            candidate = nums[i];
-        }
-        count += (nums[i] == candidate) ? 1 : -1;
-    }
-    return candidate;
+    // int count = 0;
+    // int candidate = 0;
+    // for(int i = 0; i < numsSize; i++){
+    //     if(count == 0){
+    //         candidate = nums[i];
+    //     }
+    //     count += (nums[i] == candidate) ? 1 : -1;
+    // }
+    // return candidate;
+
+    qsort(nums , numsSize , sizeof(int) , compare);
+    return nums[numsSize / 2];
 }
