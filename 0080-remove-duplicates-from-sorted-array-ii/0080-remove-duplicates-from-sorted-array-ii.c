5708ce9d9629495c32c4a5805c70dc48debc5cb0
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-09-19 12:37:44 +0530

    Time: 29 ms (54.99%), Space: 15.1 MB (88.47%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.c
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.c
@@ -1,18 +1,30 @@
 int removeDuplicates(int* nums, int numsSize) {
+    // if(numsSize <= 2){
+    //     return numsSize;
+    // }
+
+    // int prev = 1;
+    // int curr = 2;
+    // while(curr < numsSize){
+    //     if(nums[curr] == nums[prev] && nums[curr] == nums[prev - 1]){
+    //         curr++;
+    //     }else{
+    //         prev++;
+    //         nums[prev] = nums[curr];
+    //         curr++;
+    //     }
+    // }
+    // return prev + 1;
+
     if(numsSize <= 2){
         return numsSize;
     }
 
-    int prev = 1;
-    int curr = 2;
-    while(curr < numsSize){
-        if(nums[curr] == nums[prev] && nums[curr] == nums[prev - 1]){
-            curr++;
-        }else{
-            prev++;
-            nums[prev] = nums[curr];
-            curr++;
+    int k = 2;
+    for(int i = 2; i < numsSize; i++){
+        if(nums[i] != nums[k - 2]){
+            nums[k++] = nums[i];
         }
     }
-    return prev + 1;
+    return k;
 }
