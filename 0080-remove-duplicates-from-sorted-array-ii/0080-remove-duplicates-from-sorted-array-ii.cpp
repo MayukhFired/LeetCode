c501a96980e55151d3ddf86de02a006c4a420d85
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-09-19 12:34:10 +0530

    Time: 0 ms (100%), Space: 19.4 MB (96.44%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size() <= 2){
-            return nums.size();
-        }
+        // if(nums.size() <= 2){
+        //     return nums.size();
+        // }
 
-        int prev = 1;
-        int curr = 2;
-        while(curr < nums.size()){
-            if(nums[prev] == nums[curr] && nums[curr] == nums[prev - 1]){
-                curr++;
-            }else{
-                prev++;
-                nums[prev] = nums[curr];
-                curr++;
+        // int prev = 1;
+        // int curr = 2;
+        // while(curr < nums.size()){
+        //     if(nums[prev] == nums[curr] && nums[curr] == nums[prev - 1]){
+        //         curr++;
+        //     }else{
+        //         prev++;
+        //         nums[prev] = nums[curr];
+        //         curr++;
+        //     }
+        // }
+        // return prev + 1;
+
+        int i , n = nums.size();
+        int idx = 0;
+        int ans = 0;
+        for(i = 0; i < n;){
+            int j = i;
+            int cnt = 0;
+            while(j < n && nums[i] == nums[j]){
+                ++cnt;
+                ++j;
             }
+
+            for(int j = 0; j < min(cnt , 2); j++){
+                nums[idx++] = nums[i];
+            }
+            i = j;
+            ans += min(cnt , 2);
         }
-        return prev + 1;
+        return ans;
     }
 };
