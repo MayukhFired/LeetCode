72708a0698f117ee31efc7d44aff23bdfa71b5e1
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-09-23 15:15:42 +0530

    Time: 5 ms (25%), Space: 17.3 MB (37.5%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.c
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.c
@@ -1,37 +1,30 @@
 #define max(a , b) ((a) > (b) ? (a) : (b))
 
 int minOperations(int* nums, int numsSize, int x) {
-    int sum = 0;
+    int left = 0;
+    int max_len = -1;
+    int curr_sum = 0;
+    int k = 0;
     for(int i = 0; i < numsSize; i++){
-        sum += nums[i];
+        k += nums[i];
     }
 
-    int target = sum - x;
-    if(target < 0){
-        return -1;
-    }
-    if(target == 0){
+    k -= x;
+    if(k == 0){
         return numsSize;
     }
-
-    int max_len = -1;
-    int current_sum = 0;
-    int left = 0;
-
+    if(k < 0){
+        return -1;
+    }
     for(int right = 0; right < numsSize; right++){
-        current_sum += nums[right];
-        while(current_sum > target && left <= right){
-            current_sum -= nums[left];
-            left++;
+        curr_sum += nums[right];
+        while(curr_sum > k && left <= right){
+            curr_sum -= nums[left++];
         }
-        if(current_sum == target){
+        if(curr_sum == k){
             max_len = max(max_len , right - left + 1);
         }
     }
 
-    if(max_len != -1){
-        return numsSize - max_len;
-    }else{
-        return -1;
-    }
+    return max_len == -1 ? -1 : numsSize - max_len; 
 }
