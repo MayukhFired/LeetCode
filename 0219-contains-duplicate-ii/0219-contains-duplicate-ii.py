045b3cf434f2b141c89513e04b91b289534178bb
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-09-26 01:10:46 +0530

    Time: 23 ms (99.94%), Space: 36.9 MB (69.2%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -9,10 +9,19 @@ class Solution:
         #     seen.add(num)
         # return False
 
-        seen = {}
-        for i in range(len(nums)):
-            if nums[i] in seen:
-                if i - seen[nums[i]] <= k:
+        # seen = {}
+        # for i in range(len(nums)):
+        #     if nums[i] in seen:
+        #         if i - seen[nums[i]] <= k:
+        #             return True
+        #     seen[nums[i]] = i
+        # return False
+
+        if len(set(nums)) == len(nums):
+            return False
+        nums.reverse()
+        for left in range(len(nums) - 1):
+            for right in range(left + 1 , min(left + k + 1 , len(nums))):
+                if nums[left] == nums[right]:
                     return True
-            seen[nums[i]] = i
         return False
