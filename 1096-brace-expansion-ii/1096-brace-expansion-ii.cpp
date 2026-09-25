da2874bd13393ed451697e6c960e80806aab1947
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-09-26 00:42:10 +0530

    Time: 15 ms (26.34%), Space: 15.2 MB (60.71%) - LeetHub

---
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+    string expression;
+    int idx;
+
+    set<string> item(){
+        set<string> ret;
+        if(expression[idx] == '{'){
+            idx++;
+            ret = expr();
+        }else{
+            ret = {string(1 , expression[idx])};
+        }
+        idx++;
+        return move(ret);
+    }
+
+    set<string> term(){
+        set<string> ret = {""};
+        while(idx < expression.size() && (expression[idx] == '{' || isalpha(expression[idx]))){
+            auto sub = item();
+            set<string> temp;
+            for(auto& left : ret){
+                for(auto& right : sub){
+                    temp.insert(left + right);
+                }
+            }
+            ret = move(temp);
+        }
+        return move(ret);
+    }
+
+    set<string> expr(){
+        set<string> ret;
+        while(true){
+            ret.merge(term());
+            if(idx < expression.size() && expression[idx] == ','){
+                idx++;
+                continue;
+            }else{
+                break;
+            }
+        }
+        return move(ret);
+    }
+public:
+    vector<string> braceExpansionII(string expression) {
+        this->expression = expression;
+        this->idx = 0;
+        auto ret = expr();
+        return {ret.begin() , ret.end()};
+    }
+};
