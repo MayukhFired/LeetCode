e898ffb7b395228493a5b295d2f83d55fecf8d4c
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-09-27 00:11:17 +0530

    Time: 4 ms (4.1%), Space: 9.6 MB (58.53%) - LeetHub

---
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.c
@@ -0,0 +1,20 @@
+int compare(const void* a , const void* b){
+    return (*(int*)a - *(int*)b);
+}
+
+int countPairs(int* nums, int numsSize, int target) {
+    qsort(nums , numsSize , sizeof(int) , compare);
+    int left = 0;
+    int right = numsSize - 1;
+    int count = 0;
+
+    while(left < right){
+        if(nums[left] + nums[right] < target){
+            count += (right - left);
+            left++;
+        }else{
+            right--;
+        }
+    }
+    return count;
+}
